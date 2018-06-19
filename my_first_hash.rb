33efc78e15f2b602b79cd313c20af168d3976cd0
Rebecca Govert <becci.govert@gmail.com>
2018-06-19 15:40:06 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,11 +16,11 @@ def retrieval
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
-                 }
- end
+                 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  shipping_manifest["oil paintings"]
+  puts shipping_manifest["oil paintings"]
 end
+
 
 def adding
   shipping_manifest = {
